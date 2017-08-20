f52ce1606e63229bf30fd617cf25f9df965b4378
Chris Roberts <chrisrobertspdx@gmail.com>
2017-08-20 03:37:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 function shout(string) {
   return string.toUpperCase();
 }
+
+function whisperShout(string) {
+  console.log(string.toUpperCase());
+}
+
+
+function whisper(string) {
+  return string.toLowerCase();
+}
+
+function consoleWhisper(string) {
+  console.log(string.toLowerCase());
+}
